c450bd851cf76e896dea865a95222e916d2ad2a5
Eloy Delesderrier <72226220+eloydelesderrier@users.noreply.github.com>
2024-08-21 19:47:19 -0300

    Add files via upload

---
+++ Desafio1/Program.cs
@@ -0,0 +1,62 @@
+﻿using Desafio1.Models;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+ 
+// Declaração das variáveis
+decimal precoInicial = 0;
+decimal precoPorHora = 0;
+ 
+// Obtém os preços do usuário
+Console.WriteLine("Seja bem-vindo ao sistema de estacionamento!\n" +
+               "Digite o preço inicial:");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+ 
+Console.WriteLine("Agora digite o preço por hora:");
+precoPorHora = Convert.ToDecimal(Console.ReadLine());
+ 
+// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
+Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+ 
+// Loop do menu
+string opcao = string.Empty;
+bool exibirMenu = true;
+ 
+while (exibirMenu)
+{
+ Console.Clear();
+ Console.WriteLine("Digite a sua opção:");
+ Console.WriteLine("1 - Cadastrar veículo");
+ Console.WriteLine("2 - Remover veículo");
+ Console.WriteLine("3 - Listar veículos");
+ Console.WriteLine("4 - Encerrar");
+ 
+ switch (Console.ReadLine())
+ {
+     case "1":
+        es.AdicionarVeiculo();
+         break;
+ 
+     case "2":
+        es.RemoverVeiculo();
+         break;
+ 
+     case "3":
+        es.ListarVeiculos();
+        
+ 
+ break;
+ 
+     case "4":
+         exibirMenu = false;
+         break;
+ 
+     default:
+         Console.WriteLine("Opção inválida");
+         break;
+ }
+ 
+ Console.WriteLine("Pressione uma tecla para continuar");
+ Console.ReadLine();
+}
+ 
+Console.WriteLine("O programa se encerrou");
